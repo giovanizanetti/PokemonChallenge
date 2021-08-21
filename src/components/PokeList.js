0ae230fadd4afe4acdb2f886acd6b0acf2cb388b
giovanizanetti <zanetti.giovani@gmail.com>
2021-08-21 15:40:22 +0200

    remove comma from the last type index

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -46,6 +46,7 @@ export default function PokeList() {
     }
   }, [pokemons])
 
+  // If more them one type add comma in between
   const formatTypes = (types) => {
     return types.map((type, i) => (i !== types.length - 1 ? type.type.name + ', ' : type.type.name))
   }
